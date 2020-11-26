ba920941ee6ed2378c276c2e3b6f0e68fb296418
Sona <sona.juhasova@gmail.com>
2020-11-26 13:28:31 +0100

    sliders

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function ipValue() {
+    let ipapers = document.querySelector('#ip-slider').value;
+    document.querySelector('.ipaper-slider-value').innerHTML = ipapers;
+}
+
+function visValue() {
+    let visitors = document.querySelector('#vis-slider').value;
+    document.querySelector('.visitors-slider-value').innerHTML = visitors + '000';
+
+}
+
+function calcPrice {
+    
+}
