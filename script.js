35573ea4cd1a50a72b4d4f3bc47b8a93ef448c57
Sona <sona.juhasova@gmail.com>
2020-11-26 15:19:49 +0100

    calc edited

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function ipValue() {
     let ipapers = document.querySelector('#ip-slider').value;
     document.querySelector('.ipaper-slider-value').innerHTML = ipapers;
+    calcPrice();
 }
 
 function visValue() {
     let visitors = document.querySelector('#vis-slider').value;
     document.querySelector('.visitors-slider-value').innerHTML = visitors + '000';
-
+calcPrice();
 }
 
 
