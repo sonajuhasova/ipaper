bed777529c4cff88b5383b4b00fbe6e3ace06efe
Sona <sona.juhasova@gmail.com>
2020-11-26 16:35:45 +0100

    fonts

--- font-script.js
+++ font-script.js
@@ -2,13 +2,20 @@ function renderText() {
     let text = document.getElementById('text-input').value;
     let width = document.getElementById('width').value;
     let height = document.getElementById('height').value;    
-    let font = document.getElementById('fonts').value; 
-    let transformed = ((height / 100) * width) / 100;
-    console.log(transformed);
-    
+    let font = document.getElementById('fonts').value;  
+
+     
     document.getElementById('rendered').innerHTML = text;
     document.getElementById('rendered').style.width = width +'px';
     document.getElementById('rendered').style.fontFamily = font;
     document.getElementById('rendered').style.fontSize = height + 'px';
+    document.getElementById('rendered').style.heigth = height + 'px'; 
+
+    while (document.getElementById('rendered').scrollWidth > width) {
+        height = height - 1;
+         
+        document.getElementById('rendered').style.fontSize = height + 'px';
+        
+     }
 
 }
